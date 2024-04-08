96cc02651f0cb06e540c61e9096070a11f866ec4
Stewart Axel <staxegolo1@gmail.com>
2024-04-07 21:42:26 -0600

    programando la parte basica del juego

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ centuries, but also the leap into electronic
 typesetting, remaining essentially unchanged. 
 It was popularised in the 1960s with the release of
  Letraset sheets containing Lorem`;
+let TIEMPO_INICIAL = 30
+let tiempoActual = TIEMPO_INICIAL 
 
- let textoParseado = textoPrueba.split(" ").map((palabra)=> palabra.split(''));
- let parrafoJuego = document.querySelector(".game p");
+let textoParseado = textoPrueba.split(" ").map((palabra)=> palabra.trim().split(''));
+let parrafoJuego = document.querySelector(".game p");
+let tiempo = document.querySelector(".tiempo");
 
 let textoTageado = textoParseado.reduce((palabrasTageadas, palabra) => 
 palabrasTageadas + 
@@ -18,7 +21,57 @@ palabrasTageadas +
 '');
 
 parrafoJuego.innerHTML += textoTageado
+tiempo.textContent = TIEMPO_INICIAL
 
+const intervalID = setInterval(() =>{
+    tiempoActual --;
+    tiempo.textContent = tiempoActual 
+    if(tiempoActual === 0)
+        clearInterval(intervalID);
+}, 1000);
+
+
+
+const $input = document.querySelector('input[type="text"]');
+let $currentWord = document.querySelector('st-word:first-child');
+let indexCurrentWord = 0;
+let $letters = $currentWord.querySelectorAll('st-letter');
+let $currentLetter = $letters[0];
+let errorFlag = 0
+
+//$input.maxLength = $letters.length;
+
+
+
+
+
+
+$input.addEventListener('input',(event) =>{
+    if(event.inputType === "deleteContentBackward"){
+        $currentLetter = (!$currentLetter)? $currentWord.querySelector('st-letter:last-child') : (($currentLetter.previousElementSibling)? $currentLetter.previousElementSibling : $currentLetter) 
+        errorFlag = ($currentLetter.classList.contains('wrong') && errorFlag)? (errorFlag - 1) : errorFlag;
+        $currentLetter.classList.remove('right','wrong')
+    }else if(event.data === ' '){
+        $currentWord.classList.add((errorFlag || $currentLetter)? 'wrong' : 'good');
+        $currentWord = $currentWord.nextElementSibling;
+        $letters = $currentWord.querySelectorAll('st-letter');
+        $currentLetter = $letters[0];
+        errorFlag = 0
+    }else if(event.inputType !== "insertCompositionText" && event.data !== ''){
+        if ($currentLetter){
+            let aux = $currentLetter
+            $currentLetter = $currentLetter.nextElementSibling;
+            if(aux.innerHTML === event.data)
+                aux.classList.add('right');
+            else{
+                aux.classList.add('wrong');
+                errorFlag++
+            }
+            
+        }
+    }
+
+});
 
 
 
